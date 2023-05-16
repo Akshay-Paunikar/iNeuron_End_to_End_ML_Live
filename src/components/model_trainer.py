9a3eebccd5aa9412e9cd68d36da4bc123d14a35c
Akshay Paunikar <akshayakki29@gmail.com>
2023-05-17 00:36:03 +0530

    model hyperparameter tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,13 +43,53 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbours Regressor": KNeighborsRegressor(),
-                "XGBoost Regressor": XGBRegressor(),
-                "CatBoost Regressor": CatBoostRegressor(verbose=False),
-                "Adaboost Regressor": AdaBoostRegressor()
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor()
             }
+            
+            params = {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }            
 
-            model_report: dict = evaluate_model(x_train=X_train, y_train=y_train, x_test=X_test, y_test=y_test, models = models)
+            model_report: dict = evaluate_model(x_train=X_train, y_train=y_train, x_test=X_test, y_test=y_test, 
+                                                models = models, param=params)
+            
+            logging.info("model hyperparameter tuning done")
 
             # to get best model score from dictionary
             best_model_score = max(sorted(model_report.values()))
@@ -64,18 +104,20 @@ class ModelTrainer:
             if best_model_score<0.6:
                 raise CustomException("No Best Model Found", sys)
 
-            logging.info("Best model found on both train and test dataset")
+            logging.info("Best model found on both train and test dataset : {}".format(best_model))
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model)
+            
+            logging.info("Using the best model found to predict on test data")
 
             predicted = best_model.predict(X_test)
-
+                        
             R2_score = r2_score(y_test, predicted)
+            logging.info("Prediction result on test data : R2_Score -> {}".format(R2_score))
+            
             return R2_score, best_model
-
 
         except Exception as e:
             raise CustomException(e, sys)
-
